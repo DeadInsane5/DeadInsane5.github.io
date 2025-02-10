2f96bbf54f54318cde77f63d0812d64ae0bebe44
DeadInsane5 <aditya.ajaykumar.2006@gmail.com>
2025-02-10 19:32:12 +0530

    Added prompt

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
 const tableCells = document.querySelectorAll(".lec")
 
 tableCells.forEach(tableCell => {
-    tableCell.addEventListener("click", function(event) {
-        alert("You clicked on: " + event.target.textContent);
+    tableCell.addEventListener("click", function (event) {
+        prompt(event)
     });
 })
 
+function prompt(event) {
+    //container
+    const container = document.querySelector(".container")
+
+    //remove previous prompt
+    container.replaceChildren("")
+
+    //prompt container div
+    const promptContainer = document.createElement("div")
+    promptContainer.classList.add("promptContainer")
+
+    //prompt
+    const prompt = document.createElement("div")
+    prompt.classList.add("prompt")
+    prompt.textContent = "This is a prompt. You clicked on: " + event.target.textContent
+
+    //no button
+    const button1 = document.createElement("button")
+    button1.classList.add("noButton")
+    button1.textContent = "No"
+
+    //yes button
+    const button2 = document.createElement("button")
+    button2.classList.add("yesButton")
+    button2.textContent = "Yes"
+
+    //append children
+    promptContainer.appendChild(prompt)
+    promptContainer.appendChild(button1)
+    promptContainer.appendChild(button2)
+
+    container.appendChild(promptContainer)
+}
