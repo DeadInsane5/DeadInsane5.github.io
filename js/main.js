ca8384061b8bddffaca6b62f83394cdce3f4911f
DeadInsane5 <aditya.ajaykumar.2006@gmail.com>
2025-02-11 20:32:53 +0530

    Fixed lab calculations

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ let attListA = {
     "SS&CS": 0,
     "IKS": 0,
     "Workshop": 0,
-    "Recess" : 0,
+    "Recess": 0,
 };
 
 let attListH = {
@@ -34,17 +34,44 @@ let attListH = {
     "SS&CS": 0,
     "IKS": 0,
     "Workshop": 0,
-    "Recess" : 0,
+    "Recess": 0,
+}
+let attListLabA = {
+    "Phy": 0,
+    "Chem": 0,
+    "Maths": 0,
+    "EG": 0,
+    "PCC": 0,
+    "PP": 0,
+    "SS&CS": 0,
+    "IKS": 0,
+}
+let attListLabH = {
+    "Phy": 0,
+    "Chem": 0,
+    "Maths": 0,
+    "EG": 0,
+    "PCC": 0,
+    "PP": 0,
+    "SS&CS": 0,
+    "IKS": 0,
 }
 
 const attContainer = document.querySelector(".attContainer");
 for (let subject in attListA) {
+    let subjectStr = subject.split(" ")
+    if (subjectStr[1] == "Lab") {
+        subject = subjectStr[0]
+    }
+
     const attItem = document.createElement("p");
-    attItem.textContent = `${subject} : ${(attListA[subject]/attListH[subject])*100}% [${attListA[subject]} out of ${attListH[subject]} lectures attended]`;
+    attItem.textContent = `${subject} : ${(attListA[subject] / attListH[subject]) * 100}% [${attListA[subject]} out of ${attListH[subject]} lectures attended] | ${attListLabA[subject]} out of ${attListLabH[subject]} labs attended`;
     attContainer.appendChild(attItem);
 }
 
 export {
     attListA,
-    attListH
+    attListH,
+    attListLabA,
+    attListLabH,
 }
